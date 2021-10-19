1fa015c5194f75505592017376688a438748737b
Marko <1995marko@gmail.com>
2021-10-19 12:33:52 +0200

    Created fma_token_dataset.py

---
+++ fma_token_dataset.py
@@ -0,0 +1,24 @@
+from torch.utils.data import Dataset
+import torch
+import pandas as pd
+import random
+
+class FMATokenDataset(Dataset):
+    def __init__(self, ds_path, length=2048):
+        self.token_tracks_ds, self.tracks_length, self.Y = torch.load(ds_path)
+        self.size = len(self.tracks_length)
+        self.output_units = self.Y.max()
+        self.length = length
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, item):
+        # Get snippet
+        start = random.randint(0, self.tracks_length[item]-self.length)
+
+        token_track = self.token_tracks_ds[item, start:start+self.length]
+        Y = self.Y.iloc[item]
+        return token_track, Y
+
+
