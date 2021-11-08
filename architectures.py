c0d7a53bdf61b3a9c66675ea9e709d5791b452e5
Marko <1995marko@gmail.com>
2021-11-08 15:04:06 +0100

    Added positional encoding with correct axis + small bugfix)

--- architectures.py
+++ architectures.py
@@ -22,24 +22,22 @@ class BERT(nn.Module):
         self.masked_language_model = nn.Linear(d_model, N_TOKENS)
 
     def forward(self, embedding):
-        #todo embedding
-        #embedding = PositionalEncoding(embedding * math.sqrt(self.d_model))
+        embedding = self.pos_encoder(embedding * math.sqrt(self.d_model))
         output = self.transformer(self.pos_encoder(embedding))
         y_cls = self.classification(output[:, 0, :])
         y_mlm = self.masked_language_model(output)
         return y_cls, y_mlm
 
 class PositionalEncoding(nn.Module):
-    #todo swap axis
     def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
 
         position = torch.arange(max_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
-        pe = torch.zeros(max_len, 1, d_model)
-        pe[:, 0, 0::2] = torch.sin(position * div_term)
-        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        pe = torch.zeros(1, max_len, d_model)
+        pe[0, :, 0::2] = torch.sin(position * div_term)
+        pe[0, :, 1::2] = torch.cos(position * div_term)
         self.register_buffer('pe', pe)
 
     def forward(self, x: Tensor) -> Tensor:
