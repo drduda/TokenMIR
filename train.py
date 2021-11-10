22d96350d9da53fc3f2c01a44b242eb5f2b22d39
Marko <1995marko@gmail.com>
2021-11-10 12:13:00 +0100

    Added train.py which is the main file for execution

---
+++ train.py
@@ -0,0 +1,25 @@
+import fire
+from fma_token_dataset import FMATokenDataModule
+import pytorch_lightning as pl
+from systems import MLMSystem, ClassificationSystem
+import architectures
+
+
+def classify(ds_path, batch_size, epochs, d_model, n_head, dim_feed, dropout, layers, token_sequence_length=1024):
+    assert d_model % n_head == 0
+
+    data_module = FMATokenDataModule(ds_path, batch_size, token_sequence_length)
+
+
+    model = architectures.BERTWithEmbedding(
+        d_model=d_model, n_head=n_head, dim_feed=dim_feed, dropout=dropout, layers=layers,
+        max_len=token_sequence_length, output_units=data_module.output_units)
+
+    mir_system = ClassificationSystem(model=model)
+    trainer = pl.Trainer(
+        max_epochs=epochs, progress_bar_refresh_rate=20, weights_summary='full')
+    trainer.fit(mir_system, data_module)
+
+
+if __name__ == '__main__':
+    fire.Fire()
