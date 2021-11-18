d8972d013276e7ee39d6387e21e01e448345cd5b
Marko <1995marko@gmail.com>
2021-11-18 14:40:27 +0100

    Changed API for specto training

--- bert_base.sh
+++ bert_base.sh
@@ -5,4 +5,4 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --partition=hpc3_gpu
 
-srun python train.py classify --ds_path=/home/student/m/mduda/data/fma_medium_tokens --batch_size=64 --token_sequence_length=1024 --epochs=300 --d_model=768 --n_head=12 --dim_feed=1024 --dropout=0.1 --layers=4 --gpus=1 --precision=16
+srun python train.py classify_from_tokens --ds_path=/home/student/m/mduda/data/fma_medium_tokens --batch_size=64 --token_sequence_length=1024 --epochs=300 --d_model=768 --n_head=12 --dim_feed=1024 --dropout=0.1 --layers=4 --gpus=1 --precision=16
--- train.py
+++ train.py
@@ -4,14 +4,29 @@ import pytorch_lightning as pl
 from systems import MLMSystem, ClassificationSystem
 import architectures
 from pytorch_lightning.loggers import TensorBoardLogger
+from spectrograms.data import FmaSpectrogramGenreDataModule
 
+def classify_from_spectograms(fma_dir, batch_size, epochs, d_model, n_head, dim_feed, dropout, layers, gpus=-1, precision=32,
+             name="default"):
+    assert d_model % n_head == 0
+    data_module =FmaSpectrogramGenreDataModule(fma_dir)
+    logger = TensorBoardLogger("tb_log", name="specto/%s" % name)
+    model = architectures.BERTWithoutEmbedding(
+        d_model=d_model, n_head=n_head, dim_feed=dim_feed, dropout=dropout, layers=layers,
+        max_len=?, output_units=16, input_units=)
+    import torch
+    mir_system = ClassificationSystem(model=model, target_dist=torch.ones(16))
+    trainer = pl.Trainer(logger=logger,
+                         max_epochs=epochs, progress_bar_refresh_rate=20, weights_summary='full', gpus=gpus,
+                         precision=precision)
+    trainer.fit(mir_system, data_module)
 
-def classify(ds_path, batch_size, epochs, d_model, n_head, dim_feed, dropout, layers, gpus=-1, precision=32,
+def classify_from_tokens(ds_path, batch_size, epochs, d_model, n_head, dim_feed, dropout, layers, gpus=-1, precision=32,
              token_sequence_length=1024, name="default"):
     assert d_model % n_head == 0
 
     data_module = FMATokenDataModule(ds_path, batch_size, token_sequence_length)
-    logger = TensorBoardLogger("tb_log", name=name)
+    logger = TensorBoardLogger("tb_log", name="tokens/%s" % name)
     model = architectures.BERTWithEmbedding(
         d_model=d_model, n_head=n_head, dim_feed=dim_feed, dropout=dropout, layers=layers,
         max_len=token_sequence_length, output_units=16)
