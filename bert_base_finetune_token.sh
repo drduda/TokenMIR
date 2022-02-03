82ea31d0ae069694fcd2c3c25536e78be77a2716
Marko <1995marko@gmail.com>
2022-02-03 12:50:18 +0100

    Updated bash scripts so that they initialize the environment themselves and new hyperparameters for thesis experiments

--- bert_base_finetune_token.sh
+++ bert_base_finetune_token.sh
@@ -5,6 +5,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --partition=hpc3_gpu
 
-read varname
+source ~/.bashrc
+conda activate TokenMIR
 
 srun python train.py finetune_from_tokens --ds_path=~/data/fma_medium_tokens --batch_size=32 --token_sequence_length=1024 --epochs=40 --backbone_path=~/backbone/backbone.ckpt --learning_rate=3e-5 --gpus=1 --precision=16 --name=$varname
