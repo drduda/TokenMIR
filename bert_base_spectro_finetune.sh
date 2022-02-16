a3d9530db4a7f381f2cf1f13cfc7cb1914f4d824
Marko Duda <43682769+drduda@users.noreply.github.com>
2022-02-16 14:53:55 +0100

    minor bugfix in shell script

--- bert_base_spectro_finetune.sh
+++ bert_base_spectro_finetune.sh
@@ -8,4 +8,4 @@
 source ~/.bashrc
 conda activate TokenMIR
 
-srun python train.py pretrain_from_spectrograms --fma_dir=~/data --backbone_path=? --learning_rate=? --batch_size=? --epochs=40 --gpus=1 --precision=16 --snippet_length=1344 --n_mels=86 --n_fft=512 --hop_length=128 --fma_subset="medium" --name=?
+srun python train.py finetune_from_spectrograms --fma_dir=~/data --backbone_path=? --learning_rate=? --batch_size=? --epochs=40 --gpus=1 --precision=16 --snippet_length=1344 --n_mels=86 --n_fft=512 --hop_length=128 --fma_subset="medium" --name=?
