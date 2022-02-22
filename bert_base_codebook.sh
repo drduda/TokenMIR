62a82563e2c8f4540ddcaaaefa2331a79ba30d02
Marko <1995marko@gmail.com>
2022-02-22 16:50:14 +0100

    another bugfix

--- bert_base_codebook.sh
+++ bert_base_codebook.sh
@@ -8,4 +8,4 @@
 source ~/.bashrc
 conda activate TokenMIR
 
-srun python train.py classify_from_tokens --ds_path=~/data/fma_medium_tokens --batch_size=64 --token_sequence_length=1344 --epochs=500 --d_model=768 --n_head=12 --dim_feed=3072 --dropout=0.1 --layers=4 --gpus=1 --precision=16 --name=?
+srun python train.py classify_from_codebooks --ds_path=~/data/fma_medium_tokens --batch_size=64 --token_sequence_length=1344 --epochs=500 --d_model=768 --n_head=12 --dim_feed=3072 --dropout=0.1 --layers=4 --gpus=1 --precision=16 --name=?
